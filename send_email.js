8d55091e1a12aa63d513175cd5b69ed467d30d24
pranavrathi1811 <87274086+prathi912@users.noreply.github.com>
2024-09-28 17:50:55 +0530

    Update send_email.js

--- send_email.js
+++ send_email.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
 require('dotenv').config();  // Load the Gmail credentials from the .env file
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: 'https://server-iota-sandy-47.vercel.app/send_email.js' })); 
 app.use(cors());
 
 // POST route for sending email
