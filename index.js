6a15df7c83a39166e5126a9067b454c590623580
pranavrathi1811 <87274086+prathi912@users.noreply.github.com>
2024-09-28 21:23:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ app.post('/send-email', async (req, res) => {
             Name: ${firstName} ${lastName}
             Email: ${email}
             Phone Number: ${phoneNumber}
-            Association: ${association}
-            Equipment: ${equipment}
+            association: ${association}
+            equipment: ${equipment}
             Best Time to Contact: ${bestTimeToContact}
             Preferred Method of Contact: ${preferredMethodOfContact}
             Additional Information: ${additionalInformation}
