3d47512749ba94dcf94bf2b37eaba1ae5a5fe66d
pranavrathi1811 <87274086+prathi912@users.noreply.github.com>
2024-09-28 19:59:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 // CORS configuration
 app.use(cors({
-  origin: 'https://pumnrdc.promate.tech',  // Allow requests from this origin
+  origin: '*',  // Allow requests from this origin
   methods: ['GET', 'POST'],                // Allow specific methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
   credentials: true  // Allow credentials if needed
