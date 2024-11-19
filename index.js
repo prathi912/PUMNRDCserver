3ad69f1a711d5b8289d4cd0ad9982a5e6c687c1c
Pranav Rathi <87274086+prathi912@users.noreply.github.com>
2024-11-19 23:02:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const express = require('express');
 const cors = require('cors');
-const sendEmailRoute = require('./api/send_email'); // Import the send email route
+const sendEmailRoute = require('./api/send_email');
 
 const app = express();
 
-// CORS setup
 const corsOptions = {
-  origin: 'https://pumnrdc-next-pranavrathi07.vercel.app', // Allow only the frontend domain
-  methods: 'GET,POST', // Allow only these methods
-  allowedHeaders: 'Content-Type, Authorization', // Allow these headers
+  origin: '*',  // Allow any origin
+  methods: 'GET,POST',
+  allowedHeaders: 'Content-Type, Authorization',
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
 };
+
+
 app.use(cors(corsOptions));
 
-// Use the send email router
 app.use('/api/send_email', sendEmailRoute);
 
-
-// Start the server
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+process.on('SIGINT', () => {
+  server.close(() => {
+    console.log('Server stopped');
+    process.exit(0);
+  });
 });
