7a80497da374100563804d5e53a23c4fcbc139c3
Pranav Rathi <87274086+prathi912@users.noreply.github.com>
2024-11-30 14:27:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ const generateRoute = require('./api/generate');
 
 const app = express();
 
-// Middleware for CORS (allowing all origins for now, adjust in production)
+// List of whitelisted domains
+const allowedOrigins = [
+  'https://micronanornd.paruluniversity.ac.in', // Add the specific origins you want to allow
+];
+
+// Middleware for CORS (only allowing whitelisted origins)
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'micronanornd.paruluniversity.ac.in'); // Allow all origins (use a specific origin in production)
+  const origin = req.headers.origin;
+
+  // Check if the request's origin is in the whitelist
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin); // Allow the origin
+  } else {
+    res.header('Access-Control-Allow-Origin', ''); // Reject if origin is not in the whitelist
+  }
+
   res.header('Access-Control-Allow-Methods', 'GET, POST'); // Allow only GET and POST methods
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow these headers
 
@@ -23,10 +36,9 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 // Routes for payment and email functionality
-app.use('/api/payment', paymentRoute); // Ensure payment route is set correctly
-app.use('/api/send_email', sendEmailRoute); // Send email route
+app.use('/api/payment', paymentRoute);
+app.use('/api/send_email', sendEmailRoute);
 app.use('/api/generate', generateRoute);
-
 
 const PORT = process.env.PORT || 3001;
 const server = app.listen(PORT, () => {
