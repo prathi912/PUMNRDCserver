ffd6395906c581e850e765295df349e3824f8a6a
Pranav Rathi <87274086+prathi912@users.noreply.github.com>
2025-04-29 11:47:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,18 +10,9 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: 'https://micronanornd.paruluniversity.ac.in', // Only allow this domain
   methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
