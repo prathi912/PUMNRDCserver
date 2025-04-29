71a153b632afd856eaff55993fd88782881361a4
prathi912 <87274086+prathi912@users.noreply.github.com>
2025-04-29 14:14:33 +0530

    rbdf

--- index.js
+++ index.js
@@ -5,29 +5,12 @@ const sendEmailRoute = require('./api/send_email');
 const app = express();
 
 const allowedOrigins = [
-<<<<<<< HEAD
-  'https://micronanornd.paruluniversity.ac.in/', // e.g., Vercel domain
-=======
   'https://micronanornd.paruluniversity.ac.in', // e.g., Vercel domain
->>>>>>> 66f6e2f478fdea17135c290c7f2543e2c8fb53a9
   'http://localhost:3000' // for local testing
 ];
 
 app.use(cors({
   origin: function (origin, callback) {
-<<<<<<< HEAD
-    // Allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-=======
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
@@ -36,7 +19,6 @@ app.use(cors({
   },
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
->>>>>>> 66f6e2f478fdea17135c290c7f2543e2c8fb53a9
 }));
 
 app.use(express.json());
