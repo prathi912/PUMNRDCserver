5c00fa2f88557392a49135d0631fddb1b9e3ee12
Dipanshu Mishra <dipanshu.a.mishra06@gmail.com>
2025-09-26 15:53:10 +0530

    fix

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(
+  "This project uses Vercel API routes. Check /api/send-email.js for the email functionality."
+);
+
+// Export a simple function (won't be used but satisfies Node.js module requirements)
+module.exports = {
+  message: "This project uses Vercel API routes",
+};
